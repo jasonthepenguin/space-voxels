ffc279c2b4ba9060cc6f275243b1f7a505007403
Jason Botterill <jasonbotterill@hotmail.com>
2025-03-12 11:24:48 +0800

    Add package and vercel helper

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   server: {
@@ -8,6 +9,11 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+      },
+    },
   },
   // Make environment variables available to the client
   define: {
